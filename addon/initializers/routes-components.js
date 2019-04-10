7eb0803895ed46e328eb7bc598b0f4938c426245
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-04-11 00:39:00 +0200

    Refactor

--- addon/initializers/routes-components.js
+++ addon/initializers/routes-components.js
@@ -1,23 +1,25 @@
 import { getOwner } from '@ember/application';
 import Route from '@ember/routing/route';
+import routeTemplate from 'ember-routes-components/templates/route-template';
 
 export function initialize() {
   Route.reopen({
     renderTemplate(controller) {
       const { routeName } = this
+      const owner = getOwner(this);
       const routeComponentName = `routes/${routeName}`;
+      const hasRouteComponentTemplate = owner.hasRegistration(`template:components/${routeComponentName}`);
+      const hasRouteComponent = owner.hasRegistration(`component:${routeComponentName}`);
 
-      if (this._hasRouteComponent(routeComponentName)) {
+      if (hasRouteComponentTemplate || hasRouteComponent) {
         controller.routeComponentName = routeComponentName;
-        return this.render('routes-components');
+
+        if (!owner.hasRegistration(`template:${routeName}`)) {
+          owner.register(`template:${routeName}`, routeTemplate);
+        }
       }
 
       return this._super(...arguments);
-    },
-
-    _hasRouteComponent(routeComponentName) {
-      const hasTemplate = getOwner(this).lookup(`template:components/${routeComponentName}`);
-      return !!(hasTemplate || getOwner(this).lookup(`components:${routeComponentName}`));
     }
   });
 }
