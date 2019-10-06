f02619fd0f081d52e2929407db55e668d70b0862
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-10-07 01:30:33 +0200

    Fix routes components discovery

--- addon/initializers/routes-components.js
+++ addon/initializers/routes-components.js
@@ -2,6 +2,7 @@ import { getOwner } from '@ember/application';
 import { set } from '@ember/object';
 import Route from '@ember/routing/route';
 import routeTemplate from 'ember-routes-components/templates/route-template';
+import { dasherize } from '@ember/string';
 
 export function initialize() {
   Route.reopen({
@@ -9,14 +10,15 @@ export function initialize() {
       this._super(...arguments);
 
       const { routeName } = this
+      const componentName = dasherize(routeName.replace(/[.]/g, '/'));
       const owner = getOwner(this);
-      const routeComponentPath = `routes/${routeName}`;
+      const routeComponentPath = `routes/${componentName}`;
       const hasRouteComponentTemplate = owner.hasRegistration(`template:components/${routeComponentPath}`);
       const hasRouteComponent = owner.hasRegistration(`component:${routeComponentPath}`);
 
       if (hasRouteComponentTemplate || hasRouteComponent) {
-        if (!owner.hasRegistration(`template:${routeName}`)) {
-          owner.register(`template:${routeName}`, routeTemplate);
+        if (!owner.hasRegistration(`template:${componentName}`)) {
+          owner.register(`template:${componentName}`, routeTemplate);
         }
 
         set(controller, 'routeComponentPath', routeComponentPath);
